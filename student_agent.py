bca14499b4fce59126a8dbb1bb1b3510769b7298
Lai Shih-Yu <akinesia112@gmail.com>
2025-03-24 16:10:04 +0800

    train 80000 episodes

--- student_agent.py
+++ student_agent.py
@@ -13,8 +13,8 @@ with open('q_table.pkl', 'rb') as f:
     loaded_dict = pickle.load(f)
     q_table_list = loaded_dict  # Replace 0 with your default value
     q_table = {k:np.array(v) for k,v in q_table_list.items()}
-    print('loaded')
-    
+
+#print('len of q_table',len(q_table.keys()))
 global stations, candidates_p,candidates_goal, pickup, last_action, last_record_action
 stations = [[0,0] for _ in range(4)]
 candidates_p = [i for i in stations]
@@ -77,45 +77,39 @@ def get_state_obs(obs,action,last_action=None):
     destination_look = destination_look and agent_pos in candidates_goal
     real_look = passenger_look if not pickup else destination_look
     if action==pickup_id and (pickup or not real_look):
-        reward_shaping -=30
+        reward_shaping -=20
     if action==drop_id and (not pickup or not real_look):
-        reward_shaping -=25
+        reward_shaping -=20
     if action == 0 and obstacle_south:
-        reward_shaping -=25
+        reward_shaping -=20
     if action == 1 and obstacle_north:
-        reward_shaping -=25
+        reward_shaping -=20
     if action == 2 and obstacle_east:
-        reward_shaping -=25
+        reward_shaping -=20
     if action == 3 and obstacle_west:
-        reward_shaping -=25
+        reward_shaping -=20
     relative_pos = (cmp(agent_pos[0],cmp_pos[0]),cmp(agent_pos[1],cmp_pos[1]))
     return (relative_pos,pickup,real_look, (obstacle_north,obstacle_south,obstacle_east,obstacle_west),last_action),reward_shaping
 
-
 def get_action(obs):
-    
     # TODO: Train your own agent
     # HINT: If you're using a Q-table, consider designing a custom key based on `obs` to store useful information.
     # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
-    #       To prevent crashes, implement a fallback strategy for missing keys. 
+    #       To prevent crashes, implement a fallback strategy for missing keys.
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
-
     global last_action,last_record_action
     state,reward = get_state_obs(obs,last_action,last_record_action)
     action_name = ['Move North','Move South','Move East','Move West','Pick Up','Drop Off']
     if state not in q_table.keys():
         print(state)
+        #assert(0)
         action = np.random.randint(action_size)
     else:
+        #print(state,action_name[np.argmax(q_table[state])])
         q_table[state] = np.array(q_table[state])
         action = np.argmax(q_table[state])
     last_action = action
     if action in [0,1,2,3]:
         last_record_action = action
     q_table[state][action] = q_table[state][action] + (reward + np.max(q_table[state])-q_table[state][action])
-
-    return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
-    # You can submit this random agent to evaluate the performance of a purely random strategy.
-
-
-
+    return action # Choose a random action
